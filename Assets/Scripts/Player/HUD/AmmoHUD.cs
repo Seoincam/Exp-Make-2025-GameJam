3bcea4f6cba00be759bee4185989386d3b163ebc
Seoincam <seoin4715@naver.com>
2026-02-08 03:08:34 +0900

    feat: Ammo HUD 로직

--- Assets/Scripts/Player/HUD/AmmoHUD.cs
+++ Assets/Scripts/Player/HUD/AmmoHUD.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using Shared.Stat;
 using UnityEngine;
@@ -7,6 +7,11 @@ namespace Player.HUD
 {
     public class AmmoHUD : MonoBehaviour
     {
+        [SerializeField] private Transform ammoContainer;
+        [SerializeField] private GameObject ammoPrefab;
+
+        private readonly List<GameObject> _ammoInstances = new();
+        
         private void Awake()
         {
             InitializeAsync().Forget();
@@ -21,7 +26,26 @@ namespace Player.HUD
 
         private void OnStatChanged(in Stat.StatChangedEventArgs args)
         {
-            
+            if (args.Type == StatType.Health)
+            {
+                int count = (int)args.NewFinalValue / 5;
+
+                // TODO: 
+                // 일단 그냥 항상 생성-재생성 함.
+                // 병목 있다면.. 풀링
+                
+                foreach (var ammo in _ammoInstances)
+                {
+                    Destroy(ammo);
+                }
+                _ammoInstances.Clear();
+
+                while (count-- > 0)
+                {
+                    var instance = Instantiate(ammoPrefab, ammoContainer);
+                    _ammoInstances.Add(instance);
+                }
+            }
         }
     }
 }
