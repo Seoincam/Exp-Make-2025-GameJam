acb82d44a23585b155684fb9a7e01a6690ba5efd
Seoincam <seoin4715@naver.com>
2026-02-08 00:44:37 +0900

    feat: 범위 효과 (일단 마늘 효과만)

--- Assets/Scripts/Shared/Stat/Effect/EffectType.cs
+++ Assets/Scripts/Shared/Stat/Effect/EffectType.cs
@@ -7,5 +7,10 @@ namespace Shared.Stat
         Stun,
         Freeze,
         Burn,
+        
+        /// <summary>
+        /// 무기 '마늘' 착용 시, 플레이어 주변에 광역 데미지 제공.
+        /// </summary>
+        GarlicAreaDamage,
     }
 }
